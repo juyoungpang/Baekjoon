c9ef9a96c1d15acf98c9e50d9d3c34410442dc30
juyoungpang <jenna990615@naver.com>
2022-04-20 20:29:56 +0900

    .

--- src/sorting/p1427_insertion.java
+++ src/sorting/p1427_insertion.java
@@ -4,7 +4,7 @@
     import java.io.*;
     import java.util.*;
 
-    public class p1427 {
+    public class p1427_insertion {
         public static void main(String[] args) throws IOException{
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
