aa9e62dff22d647151185bff19c8200876a3df85
juyoungpang <jenna990615@naver.com>
2022-05-31 12:53:27 +0900

    .

--- src/dynamic_programming/p2839_bottom.java
+++ src/dynamic_programming/p2839_bottom.java
@@ -8,6 +8,7 @@ public class p2839_bottom {
 
     static BufferedReader br;
 
+    
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
