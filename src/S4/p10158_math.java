57cf41a84b13ea5d776a56a6c102803cd5a9330e
juyoungpang <jenna990615@naver.com>
2023-01-18 21:28:22 +0900

    .

--- src/S4/p10158_math.java
+++ src/S4/p10158_math.java
@@ -1,4 +1,5 @@
 package S4;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,12 +7,12 @@ import java.util.StringTokenizer;
 
 public class p10158_math {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer tok = new StringTokenizer(br.readLine());
 		int w = Integer.parseInt(tok.nextToken());
 		int h = Integer.parseInt(tok.nextToken());
-				
+
 		tok = new StringTokenizer(br.readLine());
 		int p = Integer.parseInt(tok.nextToken());
 		int q = Integer.parseInt(tok.nextToken());
@@ -19,14 +20,16 @@ public class p10158_math {
 		int t = Integer.parseInt(br.readLine());
 
 		p = (p + t) % (2 * w);
-        q = (q + t) % (2 * h);
+		q = (q + t) % (2 * h);
 
-        if (p > w) p = 2 * w - p;
-        if (q > h) q = 2 * h - q;
+		if (p > w)
+			p = 2 * w - p;
+		if (q > h)
+			q = 2 * h - q;
 
 		StringBuilder sb = new StringBuilder();
 		sb.append(p).append(" ").append(q);
-		
+
 		System.out.println(sb);
 	}
 }
